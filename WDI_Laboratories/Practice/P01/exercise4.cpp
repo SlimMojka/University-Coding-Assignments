915ef3a9ec68799035c63be8ce35e5f2d3dc662a
Slim <93072451+SlimMojka@users.noreply.github.com>
2026-01-11 23:53:51 +0100

    more cpp exercises

---
+++ WDI_Laboratories/Practice/P01/exercise4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Napisz program, który:
+    * oblicza sumę każdego wiersza macierzy
+    * zapisuje wyniki w jednowymiarowej tablicy
+    * wykorzystuje pętle zagnieżdżone
+    */
+
+    int macierz[4][3];
+
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            
+        }
+    }
+
+}
